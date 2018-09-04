0186efbf2a02f768f33b1ac24fa02244fd9f45eb
Beto MOURA <betomoura1392@gmail.com>
2018-09-03 22:31:43 -0300

    Coloquei mais um echo e 456

---
+++ aula_1.php
@@ -0,0 +1,25 @@
+<?php
+//Essa é a primeira aula PHp
+echo "<b>Alo</b> mundo";
+
+?>
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+	body{
+
+		font-family: verdana;
+		color: #000000;
+		background: #ccff;
+
+	}
+</style>
+<head>
+	<title>Pagina teste</title>
+	
+</head>
+<body>
+	<table width="500" height="200" bgcolor="#125677"><tr><td>Testando</td></tr></table>
+
+</body>
+</html>
